77246a34f7d8a8ad63eb0a6594ae9d16336e3d53
Abdalmassih Yakeen <Abdalmassih@Abdalmassih>
2019-03-21 12:38:46 +0200

    baked users added auth

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -1,34 +1,133 @@
 <?php
+App::uses('AppController', 'Controller');
+/**
+ * Users Controller
+ *
+ * @property User $User
+ * @property PaginatorComponent $Paginator
+ */
+class UsersController extends AppController
+{
 
+/**
+ * Components
+ *
+ * @var array
+ */
+    public $components = array('Paginator');
 
-class UsersController extends AppController {
-    public $helpers = array('Html', 'Form');
-
-
-	public function index() {
-        $this->set('users', $this->User->find('all'));
+    public function beforeFilter()
+    {
+        $this->Auth->allow('register', 'add');
     }
 
-	public function view($id = null) {
-        if (!$id) {
-            throw new NotFoundException(__('Invalid post'));
-        }
-
-        $post = $this->User->findById($id);
-        if (!$post) {
-            throw new NotFoundException(__('Invalid post'));
-        }
-        $this->set('user', $post);
-	}
-
-	public function add() {
+    public function login()
+    {
         if ($this->request->is('post')) {
-            $this->Post->create();
-            if ($this->Post->save($this->request->data)) {
-                $this->Flash->success(__('Your post has been saved.'));
-                return $this->redirect(['action' => 'index']);
+            if ($this->Auth->login()) {
+                return $this->redirect($this->Auth->redirectUrl());
             }
-            $this->Flash->error(__('Unable to add your post.'));
+            $this->Session->setFlash('Invalid Username or Password!');
         }
+    }
+
+    public function logout()
+    {
+
+    }
+
+/**
+ * index method
+ *
+ * @return void
+ */
+    public function index()
+    {
+        $this->User->recursive = 0;
+        $this->set('users', $this->Paginator->paginate());
+    }
+
+/**
+ * view method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+    public function view($id = null)
+    {
+        if (!$this->User->exists($id)) {
+            throw new NotFoundException(__('Invalid user'));
+        }
+        $options = array('conditions' => array('User.' . $this->User->primaryKey => $id));
+        $this->set('user', $this->User->find('first', $options));
+    }
+
+/**
+ * add method
+ *
+ * @return void
+ */
+    public function add()
+    {
+        if ($this->request->is('post')) {
+            $this->User->create();
+
+            //hash password
+            $this->request->data['User']['password'] = AuthComponent::password($this->request->data['User']['password']);
+
+            if ($this->User->save($this->request->data)) {
+                $this->Flash->success(__('The user has been saved.'));
+                return $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Flash->error(__('The user could not be saved. Please, try again.'));
+            }
+        }
+    }
+
+/**
+ * edit method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+    public function edit($id = null)
+    {
+        if (!$this->User->exists($id)) {
+            throw new NotFoundException(__('Invalid user'));
+        }
+        if ($this->request->is(array('post', 'put'))) {
+            if ($this->User->save($this->request->data)) {
+                $this->Flash->success(__('The user has been saved.'));
+                return $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Flash->error(__('The user could not be saved. Please, try again.'));
+            }
+        } else {
+            $options = array('conditions' => array('User.' . $this->User->primaryKey => $id));
+            $this->request->data = $this->User->find('first', $options);
+        }
+    }
+
+/**
+ * delete method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+    public function delete($id = null)
+    {
+        if (!$this->User->exists($id)) {
+            throw new NotFoundException(__('Invalid user'));
+        }
+        $this->request->allowMethod('post', 'delete');
+        if ($this->User->delete($id)) {
+            $this->Flash->success(__('The user has been deleted.'));
+        } else {
+            $this->Flash->error(__('The user could not be deleted. Please, try again.'));
+        }
+        return $this->redirect(array('action' => 'index'));
     }
 }
